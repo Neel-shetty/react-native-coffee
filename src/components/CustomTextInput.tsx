d13d194cb4c8910744a2d0f1ac126375289e5d40
Neel <neelnarayanshetty@gmail.com>
2022-12-05 23:53:28 +0530

    sign in screen layout

---
+++ src/components/CustomTextInput.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, TextInput, View } from "react-native";
+import React from "react";
+
+const CustomTextInput = () => {
+  return (
+    <View>
+      <TextInput />
+    </View>
+  );
+};
+
+export default CustomTextInput;
+
+const styles = StyleSheet.create({});
--- src/screens/SignUpScreens/SignInScreen.tsx
+++ src/screens/SignUpScreens/SignInScreen.tsx
@@ -1,10 +1,25 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import BackArrow from "../../components/BackArrow";
+import CustomTextInput from "../../components/CustomTextInput";
 
 const SignInScreen = () => {
   return (
     <View>
-      <Text>SignInScreen</Text>
+      <View>
+        <BackArrow />
+      </View>
+      <View>
+        <Text>Sign in</Text>
+        <Text>Welcome back</Text>
+      </View>
+      <View>
+        <CustomTextInput />
+        <CustomTextInput />
+      </View>
+      <View>
+        <Text>Forgot Password?</Text>
+      </View>
     </View>
   );
 };
