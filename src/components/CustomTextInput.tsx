0b9422fc768286b33517e4267e8b7df072be9f4a
Neel <neelnarayanshetty@gmail.com>
2022-12-06 17:40:33 +0530

    custom input

--- src/components/CustomTextInput.tsx
+++ src/components/CustomTextInput.tsx
@@ -1,14 +1,76 @@
 import { StyleSheet, Text, TextInput, View } from "react-native";
 import React from "react";
+import { width } from "../constants/Layout";
+import { Ionicons } from "@expo/vector-icons";
 
-const CustomTextInput = () => {
+interface inputTypes {
+  placeholder: string;
+  icon: string;
+}
+
+const CustomTextInput = ({ placeholder, icon }: inputTypes) => {
   return (
-    <View>
-      <TextInput />
+    <View style={styles.root}>
+      {icon === "mail" ? (
+        <Ionicons
+          name="mail-outline"
+          size={24}
+          style={styles.icon}
+          color="#001833"
+        />
+      ) : null}
+      {icon === "password" ? (
+        <Ionicons
+          name="lock-closed-outline"
+          style={styles.icon}
+          color="#001833"
+          size={24}
+        />
+      ) : null}
+      <View style={styles.inputContainer}>
+        <View style={styles.divider} />
+        <TextInput
+          style={styles.input}
+          placeholder={placeholder}
+          placeholderTextColor={"#C1C7D0"}
+        />
+      </View>
     </View>
   );
 };
 
 export default CustomTextInput;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  root: {
+    borderBottomWidth: 1,
+    borderColor: "#C1C7D0",
+    width: width * 0.9,
+    alignSelf: "center",
+    height: 29,
+    // backgroundColor: "pink",
+    margin: 10,
+    flexDirection: "row",
+  },
+  input: {
+    // backgroundColor: "violet",
+    width: width * 0.8,
+    fontFamily: "poppins-regular",
+  },
+  inputContainer: {
+    paddingLeft: 5,
+    // borderLeftWidth: 1,
+    // borderColor: "#C1C7D0",
+    paddingBottom: 2,
+    flexDirection: "row",
+  },
+  icon: {
+    paddingRight: 0,
+  },
+  divider: {
+    borderLeftWidth: 1,
+    height: 25,
+    width: 5,
+    borderColor: "#C1C7D0",
+  },
+});
--- src/screens/SignUpScreens/SignInScreen.tsx
+++ src/screens/SignUpScreens/SignInScreen.tsx
@@ -1,33 +1,44 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ScrollView } from "react-native";
 import React from "react";
 import BackArrow from "../../components/BackArrow";
 import CustomTextInput from "../../components/CustomTextInput";
 import Colors from "../../constants/Colors";
+import NextArrow from "../../components/NextArrow";
 
 const SignInScreen = () => {
   return (
-    <View>
-      <View>
+    <ScrollView style={styles.root} showsVerticalScrollIndicator={false}>
+      <View style={styles.header}>
         <BackArrow />
       </View>
-      <View>
+      <View style={styles.top}>
         <Text style={styles.title}>Sign in</Text>
         <Text style={styles.subtitle}>Welcome back</Text>
       </View>
-      <View>
-        <CustomTextInput />
-        <CustomTextInput />
+      <View style={styles.inputContainer}>
+        <CustomTextInput placeholder={"Email"} icon={"mail"} />
+        <CustomTextInput placeholder={"Password"} icon={"password"}/>
       </View>
-      <View>
+      <View style={styles.linkContainer}>
         <Text style={styles.link}>Forgot Password?</Text>
       </View>
-    </View>
+      <View style={styles.arrowContainer}>
+        <NextArrow />
+      </View>
+      <View style={styles.subtitleContainer}>
+        <Text>New member?</Text>
+        <Text>Sign up</Text>
+      </View>
+    </ScrollView>
   );
 };
 
 export default SignInScreen;
 
 const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+  },
   title: {
     fontFamily: "poppins-medium",
     fontSize: 22,
@@ -42,5 +53,23 @@ const styles = StyleSheet.create({
     fontFamily: "poppins-medium",
     fontSize: 14,
     color: Colors.primary100,
+  },
+  header: {
+    flex: 1,
+  },
+  top: {
+    flex: 1,
+  },
+  inputContainer: {
+    flex: 1,
+  },
+  linkContainer: {
+    flex: 1,
+  },
+  arrowContainer: {
+    flex: 1,
+  },
+  subtitleContainer: {
+    flex: 1,
   },
 });
