aa26c32144b662a4983581f607bebb4effe24736
Neel <neelnarayanshetty@gmail.com>
2022-12-09 17:59:34 +0530

    data

---
+++ assets/dummyData/MenuData.tsx
@@ -0,0 +1,26 @@
+export const menu = [
+  {
+    id: 1,
+    name: "Americano",
+    image: require("../images/Home/coffee1.png"),
+    cost: "$3",
+  },
+  {
+    id: 2,
+    name: "Cappuccino",
+    image: require("../images/Home/coffee1.png"),
+    cost: "$3",
+  },
+  {
+    id: 3,
+    name: "Mocha",
+    image: require("../images/Home/coffee1.png"),
+    cost: "$5",
+  },
+  {
+    id: 4,
+    name: "Flat white",
+    image: require("../images/Home/coffee1.png"),
+    cost: "$5",
+  },
+];
