364dcde68bd31ad4ec3c2f99ddffaee7bf4fa97c
Neel <neelnarayanshetty@gmail.com>
2023-01-01 00:12:59 +0530

    idk

--- src/screens/MainScreens/RewardScreen.tsx
+++ src/screens/MainScreens/RewardScreen.tsx
@@ -1,14 +1,17 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
 
 const RewardScreen = () => {
   return (
     <View style={styles.root}>
       <Text style={styles.text}>RewardScreen</Text>
     </View>
-  )
+  );
+};
 
+export default RewardScreen;
 
-export default RewardScreen
-
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  root:{},
+  text:{},
+});
